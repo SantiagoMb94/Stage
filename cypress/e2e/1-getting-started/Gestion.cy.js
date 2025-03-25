c8d039c5347946a1f1a20692196a3ec17d0de0f7
Santiago M <49466633+SantiagoMb94@users.noreply.github.com>
2025-03-25 16:17:43 -0500

    Branch Gestion fix

--- cypress/e2e/1-getting-started/Gestion.cy.js
+++ cypress/e2e/1-getting-started/Gestion.cy.js
@@ -8,47 +8,55 @@ describe('Login - Etapa 1', () => {
       cy.get('.suma-responsive-1_89 > .suma-flex-direction-column > .suma-background-transparent')
         .should('be.visible')
         .click();
-      cy.wait(500);
+        cy.wait(500);
   
       cy.get('sc-select-third > .suma-width-full')
         .should('be.visible')
         .select('Colombia');
-      cy.wait(500);
+        cy.wait(500);
   
       cy.get('.suma-gap-y-35 > :nth-child(2) > .suma-width-full')
         .should('be.visible')
         .type('223456789');
-      cy.wait(500);
+        cy.wait(500);
   
       cy.get('.suma-border-bottom-default > .suma-width-full')
         .should('be.visible')
         .type('Meteoro2021*+');
-      cy.wait(500);
+        cy.wait(500);
   
       cy.get('.suma-width-177')
         .should('be.visible')
         .click();
-      cy.wait(500);
+        cy.wait(500);
         
       cy.get('.suma-flex-between > :nth-child(3)')
         .should('be.visible')
         .click();
-      cy.wait(500);
+        cy.wait(500);
 
       cy.get('.suma-height-24')
         .should('be.visible')
         .click();
-      cy.wait(1000);
+        cy.wait(500);
 
       cy.get('.profile-nav-items-container > :nth-child(1)')
+        .should('exist')
         .should('be.visible')
         .click();
-      cy.wait(1000);
+        cy.wait(500);
+
+        cy.get(':nth-child(1) > .profile-nav-text > .nav-title')
+        .should('exist')
+        .should('be.visible')
+        .click();
+        cy.wait(500);
 
       cy.get('.suma-center-between > .white_brand_background')
+        .should('exist')
         .should('be.visible')
         .click();
-      cy.wait(1000);
+        cy.wait(500);
 
       
       const timestamp = new Date().getTime(); 
@@ -58,11 +66,17 @@ describe('Login - Etapa 1', () => {
       cy.get('.suma-wrap-between > :nth-child(1) > .suma-width-245')
         .should('be.visible')
         .type(uniqueTestName);
-      cy.wait(500); 
+        cy.wait(500); 
 
       cy.get('suma-search-staff > .suma-position-relative > .suma-width-height-full')
         .should('be.visible')
         .click();
+        cy.wait(500);
+
+        cy.get('suma-search-staff > .suma-position-relative > .suma-width-height-full')
+        .should('be.visible')
+        cy.get('.ant-select-item-option-content')
+        cy.wait(500);
 
         cy.get('.ant-select-item-option-content > .suma-display-flex')
         .should('be.visible')
